849017d2c5eddf4b128a8b38c59e9126f0235970
Nukr <nukrs.w@gmail.com>
2015-01-16 15:28:57 +0800

    refactor

--- index.js
+++ index.js
@@ -1,134 +1,171 @@
-var keyMirror = require('keymirror');
 var request = require('koa-request');
-
-var classes = keyMirror({
-    "Chats": null,
-    "Comments": null,
-    "Follows": null,
-    "Likes": null,
-    "Messages": null,
-    "Orders": null,
-    "Products": null
-});
-
-var buildinService = keyMirror({
-    "users": null,
-    "roles": null,
-    "files": null,
-    "events": null,
-    "push": null,
-    "installations": null,
-    "functions": null,
-    "jobs": null
-});
+var _ = require('underscore');
 
 /**
  * constructor
  *
  */
-function Parse(appId, restApiKey) {
-    this.parseApi = 'https://api.parse.com/1';
+function Parse(appId, restApiKey, sessionToken) {
     this.appId = appId;
     this.restApiKey = restApiKey;
-    this.options = {
-        headers: {
-            'X-Parse-Application-Id': this.appId,
-            'X-Parse-REST-API-Key': this.restApiKey,
-            'Content-Type:': 'application/json'
-        }
-    };
+    this.sessionToken = _.isUndefined(sessionToken) ? null : sessionToken;
 };
 
-Parse.prototype.classes = function (className) {
-    if (classes[className] === undefined) {
-        return new Error('className dose not exist');
-    }
+Parse.prototype = {
 
-    var that = this;
+    API_BASE_URL: 'https://api.parse.com',
+    appId: null,
+    restApiKey: null,
+    masterKey: null,
+    sessionToken: null,
 
-    return {
-        get: function (objectId, qs) {
-            qs = qs || '';
-            that.options.url = that.parseApi + '/classes/' + className + '/' + objectId + qs;
-            return request(that.options)
-        },
+    classes: function (className) {
+        var that = this;
+        return {
+            get: function (objectId, qs) {
+                qs = qs || '';
+                return that._request({
+                    method: 'GET',
+                    url: '/1/classes/' + className + '/' + objectId,
+                    params: qs
+                });
+            },
 
-        getAll: function (qs) {
-            qs = qs || '';
-            that.options.url = that.parseApi + '/classes/' + className + qs;
-            return request(that.options)
-        },
+            getAll: function (qs) {
+                qs = qs || '';
+                return that._request({
+                    method: 'GET',
+                    url: '/1/classes/' + className,
+                    params: qs
+                });
+            },
 
-        create: function () {
-            console.log(className);
-            console.log('i am create()');
-        },
+            create: function (data) {
+                return that._request({
+                    method: 'POST',
+                    url: '/1/classes/' + className,
+                    params: data
+                });
+            },
 
-        del: function () {
-            console.log(className);
-            console.log('i am del()');
-        },
+            del: function () {
+                console.log(className);
+                console.log('i am del()');
+            },
 
-        update: function () {
-            console.log(className);
-            console.log('i am update()');
+            update: function () {
+                console.log(className);
+                console.log('i am update()');
+            }
+
+        }
+    },
+
+    users: function () {
+        var that = this;
+        return {
+            login: function (userData) {
+                that.options.url = that.parseApi + '/login?username=' + userData.username + '&password=' + userData.password
+                return request(that.options);
+            },
+
+            requestPasswordReset: function (email) {
+                that.options.url = that.parseApi + '/return requestPasswordReset';
+                that.options.method = 'POST';
+                that.options.qs = {email: email};
+                return request(that.options);
+            },
+
+            create: function (userData) {
+                that.options.method = 'POST';
+                that.options.url = that.parseApi + '/users';
+                that.options.json = userData;
+                return request(that.options);
+            },
+
+            get: function (objectId) {
+                that.options.url = that.parseApi + '/users/' + objectId;
+                return request(that.options);
+            },
+
+            getAll: function () {
+                that.options.url = that.parseApi + '/users';
+                return request(that.options);
+            },
+
+            verify: function (sessToken) {
+                that.options.url = that.parseApi + '/users/me'
+                that.options.headers['X-Parse-Session-Token'] = sessToken;
+                return request(that.options);
+            },
+
+            update: function (userData) {
+                that.options.method = 'PUT';
+                that.options.url = that.parseApi + '/users/' + userData.objectId;
+                that.options.qs = userData;
+                that.options.headers['X-Parse-Session-Token'] = sessToken;
+                return request(that.options);
+            },
+
+            del: function (userData) {
+                that.options.method = 'DELETE';
+                that.options.url = that.parseApi + '/users/' + objectId;
+                that.options.headers['X-Parse-Session-Token'] = sessToken;
+                return request(that.options);
+            }
+        }
+    },
+
+    stringifyParamValues: function (params) {
+        if (!params || _.isEmpty(params))
+          return null;
+        var values = _(params).map(function(value, key) {
+          if (_.isObject(value) || _.isArray(value))
+            return JSON.stringify(value);
+          else
+            return value;
+        });
+        var keys = _(params).keys();
+        var ret = {};
+        for (var i = 0; i < keys.length; i++)
+          ret[keys[i]] = values[i];
+        return ret;
+    },
+
+    _request: function (opts) {
+        opts = _.extend({
+            method: 'GET',
+            url: null,
+            params: null,
+            body: null,
+            headers: null,
+        }, opts)
+
+        var reqOpts = {
+            method: opts.method,
+            headers: {
+                'X-Parse-Application-Id': this.appId,
+                'X-Parse-REST-API-Key': this.restApiKey
+            }
+        }
+        if (opts.headers) _.extend(reqOpts.headers, opts.headers);
+        if (opts.params) {
+            if (opts.method === 'GET') {
+                opts.params = this.stringifyParamValues(opts.params);
+            }
+
+            var key = 'qs';
+            if (opts.method === 'POST' || opts.method === 'PUT') {
+                key = 'json';
+            }
+            reqOpts[key] = opts.params
+        } else if (opts.body) {
+            reqOpts[key] = opts.body;
         }
 
-    }
-};
+        reqOpts['url'] = this.API_BASE_URL + opts.url
 
-Parse.prototype.users = function () {
-    var that = this
-    return {
-        login: function (userData) {
-            that.options.url = that.parseApi + '/login?username=' + userData.username + '&password=' + userData.password
-            return request(that.options);
-        },
-
-        requestPasswordReset: function (email) {
-            that.options.url = that.parseApi + '/return requestPasswordReset';
-            that.options.method = 'POST';
-            that.options.qs = {email: email};
-            return request(that.options);
-        },
-
-        create: function (userData) {
-            that.options.method = 'POST';
-            that.options.url = that.parseApi + '/users';
-            that.options.qs = userData;
-            return request(that.options);
-        },
-
-        get: function (objectId) {
-            that.options.url = that.parseApi + '/users/' + objectId;
-            return request(that.options);
-        },
-
-        getAll: function () {
-            that.options.url = that.parseApi + '/users';
-            return request(that.options);
-        },
-
-        verify: function (sessToken) {
-            that.options.url = that.parseApi + '/users/me'
-            that.options.headers['X-Parse-Session-Token'] = sessToken;
-            return request(that.options);
-        },
-
-        update: function (userData) {
-            that.options.method = 'PUT';
-            that.options.url = that.parseApi + '/users/' + userData.objectId;
-            that.options.qs = userData;
-            that.options.headers['X-Parse-Session-Token'] = sessToken;
-            return request(that.options);
-        },
-
-        del: function (userData) {
-            that.options.method = 'DELETE';
-            that.options.url = that.parseApi + '/users/' + objectId;
-            that.options.headers['X-Parse-Session-Token'] = sessToken;
-            return request(that.options);
-        }
+        return request(reqOpts)
     }
 }
 
